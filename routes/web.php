e04229c37a357ef2dc6f7da56a1f6d5ba6ad54a5
Gabucho <gcoria@santafe.gov.ar>
2021-08-03 14:50:56 +0200

    mi primer commit

---
+++ routes/web.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware(['auth'])->group(function(){
+  //Roles
+  Route::post('roles/store','RoleController@store')->name('roles.store')
+   ->middleware('permission:roles.create'); // busca permission en archivo Kernel
+
+   Route::get('roles','RoleController@index')->name('roles.index')
+   ->middleware('permission:roles.index');
+   
+   Route::get('roles/create','RoleController@create')->name('roles.create')
+   ->middleware('permission:roles.create');
+   
+   Route::put('roles/{role}','RoleController@update')->name('roles.update')
+   ->middleware('permission:roles.edit');
+   
+   Route::get('roles/{role}','RoleController@show')->name('roles.show')
+   ->middleware('permission:roles.show');
+   
+   Route::delete('roles/{role}','RoleController@destroy')->name('roles.destroy')
+   ->middleware('permission:roles.destroy');
+   
+   Route::get('roles/{role}}/edit','RoleController@edit')->name('roles.edit')
+   ->middleware('permission:roles.edit');
+   ////////////////////////////////////////////////////////////////////////
+   
+   //Productos
+  Route::post('products/store','ProductController@store')->name('products.store')
+  ->middleware('permission:products.create'); // busca permission en archivo Kernel
+
+  Route::get('products','ProductController@index')->name('products.index')
+  ->middleware('permission:products.index');
+  
+  Route::get('products/create','ProductController@create')->name('products.create')
+  ->middleware('permission:products.create');
+  
+  Route::put('products/{product}','ProductController@update')->name('products.update')
+  ->middleware('permission:products.edit');
+  
+  Route::get('products/{product}','ProductController@show')->name('products.show')
+  ->middleware('permission:products.show');
+  
+  Route::delete('products/{product}','ProductController@destroy')->name('products.destroy')
+  ->middleware('permission:products.destroy');
+  
+  Route::get('products/{product}/edit','ProductController@edit')->name('products.edit')
+  ->middleware('permission:products.edit');
+  ///////////////////////////////////////////////////////////////////////////////////////////
+
+  //Usuarios
+ // Route::post('users/store','ProductController@store')->name('users.store')
+ // ->middleware('permission:users.create'); // busca permission en archivo Kernel
+
+  Route::get('users','UserController@index')->name('users.index')
+  ->middleware('permission:users.index');
+  
+ // Route::get('users/create','UserController@create')->name('users.create')
+ // ->middleware('permission:users.create');
+  
+  Route::put('users/{user}','UserController@update')->name('users.update')
+  ->middleware('permission:users.edit');
+  
+  Route::get('users/{user}','UserController@show')->name('users.show')
+  ->middleware('permission:users.show');
+  
+  Route::delete('users/{user}','UserController@destroy')->name('users.destroy')
+  ->middleware('permission:users.destroy');
+  
+  Route::get('users/{user}/edit','UserController@edit')->name('users.edit')
+  ->middleware('permission:users.edit');
+
+  });
+
